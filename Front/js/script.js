792626faa09b99ac63e26d2af0c9c3d8ac8b168a
Amarsalah-31 <amarnawal22@gmail.com>
2021-07-08 17:35:59 +0200

    update

---
+++ Front/js/script.js
@@ -0,0 +1,130 @@
+// À charger pendant que le site charge
+$(() => {
+    getAllCountries();
+    $("#btnShowData").click(handleClick);
+    $("#browser").click(getRegionInfo);
+    radioButton();
+    reset();
+})
+
+
+// Gestion du bouton avec l'option radio
+function handleClick() {
+
+    if ($("input[name=radio]:checked").val() === "country") {
+        getCountryInfo();
+    } else {
+        getCapitalInfo();
+    }
+
+}
+
+// Option radio
+function radioButton() {
+    $("input[name=radio]:radio").click(() => {
+        $("#text").attr("placeholder", $("#country:checked").data("hint"));
+        $("#text").attr("placeholder", $("#capital:checked").data("hint"));
+
+    })
+}
+
+function reset() {
+    $("#btnReset").click(() => {
+        document.location.reload();
+    })
+}
+
+// Récupérer tous les noms de pays
+let countriesNames = [];
+
+function getAllCountries() {
+
+    $.ajax({
+        url: "http://localhost:8000/all",
+        success: function(res) {
+            const countries = res.data;
+            let list = "";
+
+            countries.forEach(country => {
+                list += `<li>${country}</li>`
+            });
+        
+            $(".data").html(list);
+        }
+    });
+};
+
+
+
+// Récupérer les infos par pays
+function getCountryInfo() {
+
+    let country = $("input").val();
+    $.ajax({
+        url: `http://localhost:8000/${country}`,
+        success: function (res) {
+            if (res.status === "success") {
+                $(".data").html(
+                    `<img class="flag" src=${res.data[0].flag} alt="flag of ${res.data[0].name}">
+                    <li>Country: ${res.data[0].name}</li> 
+                    <li>Capital: ${res.data[0].capital}</li>
+                    <li>Currency: ${res.data[0].currencies[0].name}</li>
+                    <li>Continent: ${res.data[0].region}</li>` 
+                );
+            } else {
+                alert("This country does not exist");
+            }
+            
+        }
+    })
+}
+
+
+
+// Récupérer les capitales
+function getCapitalInfo () {
+    let capital = $("input").val();
+    $.ajax({
+        url: `http://localhost:8000/country/${capital}`,
+        success: function (res) {
+            if (res.status === "success") {
+                $(".data").html(
+                    `<li>Capital: ${res.data[0].capital}</li>
+                    <li>Country: ${res.data[0].name}</li> 
+                    <li>Continent: ${res.data[0].region}</li>
+                    <li>Currency: ${res.data[0].currencies[0].name}</li> 
+                    <img class="flag" src=${res.data[0].flag} alt="flag of ${res.data[0].name}">`
+                );
+            } else {
+                alert("This capital does not exist")
+            }
+            
+        }
+    })
+}
+
+
+
+// Récupérer les continents
+function getRegionInfo () {
+    let regionInput = $("#region").val();
+
+    $.ajax({
+        url: `http://localhost:8000/region/${regionInput}`,
+        success: function (res) {
+            if (res.status === "success") {
+                const region = res.data;
+                let list = "";
+                console.log(region);
+                region.forEach(country => {
+                    list += `<li>${country.name}</li>`
+                });
+        
+                $(".data").html(list);
+            } else {
+                alert("This region does not exist")
+            }
+            
+        }
+    })
+}
---
+++ api/server.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const PORT = 8000;
+
+const countriesData = require("./countriesData.json");
+
+
+// Route initiale
+app.get("/all", (req, res) => {
+    countriesNames = countriesData.map(country => {
+        return country.name;
+    });
+    res.json({
+        status: "success",
+        data: countriesNames
+    });
+});
+
+// Route pays
+app.get("/:country", (req, res) => {
+    let countryName = req.params.country;
+    countryName = countryName.charAt(0).toUpperCase() + countryName.slice(1).toLowerCase();
+
+    let status = "";
+
+    let countryInfo = countriesData.filter((country) => countryName === country.name)
+
+    if (countryInfo.length === 0) {
+        status = "error";
+    } else {
+        status = "success";
+    }
+
+    res.json({
+        status: status,
+        data: countryInfo
+    });
+});
+
+// Route capitale
+app.get("/country/:capital", (req, res) => {
+    let capitalName = req.params.capital;
+    capitalName = capitalName.charAt(0).toUpperCase() + capitalName.slice(1).toLowerCase();
+
+    let status = "";
+
+    let capitalInfo = countriesData.filter((country) => capitalName === country.capital)
+
+    if (capitalInfo.length === 0) {
+        status = "error";
+    } else {
+        status = "success";
+    }
+
+    res.json({
+        status: status,
+        data: capitalInfo
+    })
+
+})
+
+
+// Route region
+app.get("/region/:region", (req, res) => {
+    let regionName = req.params.region;
+    regionName = regionName.charAt(0).toUpperCase() + regionName.slice(1).toLowerCase();
+
+    let status = "";
+
+    let regionInfo = countriesData.filter((country) => regionName === country.region)
+
+    if (regionInfo.length === 0) {
+        status = "error";
+    } else {
+        status = "success";
+    }
+
+    res.json({
+        status: status,
+        data: regionInfo
+    })
+
+})
+
+
+
+
+// Démarrage serveur
+app.listen(PORT, () => {
+    console.log(`Listening to PORT ${PORT}`);
+});
